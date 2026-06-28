5fb9dbbeac989e3f3aa894d75b00878765545b8e
Zundal <ajs15010120@gmail.com>
2026-06-29 08:05:45 +0900

    Updates

--- assets/BackgroundFX-D_oXjieh.js
+++ assets/BackgroundFX-D_oXjieh.js
@@ -1,4 +1,4 @@
-import{u as p,j as a,r as l}from"./index-BX7qo2HK.js";import{C as v,B as x,a as u,b as d,u as h,A as g}from"./react-three-fiber.esm-FsmaaCXV.js";const S=`
+import{u as p,j as a,r as l}from"./index-fX-D5W8B.js";import{C as v,B as x,a as u,b as d,u as h,A as g}from"./react-three-fiber.esm-CaMsn2mx.js";const S=`
 uniform float uTime;
 uniform float uMotion;
 attribute float aSeed;
