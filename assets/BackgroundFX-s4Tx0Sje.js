a787921fa0b3af9be2bbaafe2f8aadf1dbec0e74
Zundal <ajs15010120@gmail.com>
2026-07-01 11:34:23 +0900

    Updates

--- assets/BackgroundFX-s4Tx0Sje.js
+++ assets/BackgroundFX-s4Tx0Sje.js
@@ -1,4 +1,4 @@
-import{u as p,j as a,r as l}from"./index-FwA6KnLf.js";import{C as v,B as x,a as u,b as d,u as h,A as g}from"./react-three-fiber.esm-DKxcDj-7.js";const S=`
+import{u as p,j as a,r as l}from"./index-uHBt_fOz.js";import{C as v,B as x,a as u,b as d,u as h,A as g}from"./react-three-fiber.esm-B_HYbn-R.js";const S=`
 uniform float uTime;
 uniform float uMotion;
 attribute float aSeed;
